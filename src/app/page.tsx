bf1b1ab9139afe8ccfc1bf94c028bbf7943c05a2
career-yashaswee <career.yashaswee@gmail.com>
2025-09-26 02:07:14 +0530

    fix(landing)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
-"use client";
-
 import React from "react";
 import Link from "next/link";
-import MovingBorderButton from "@/components/blocks/moving-border-button";
-import LanguageSwitcher from "@/components/common/language-switcher";
+import dynamic from "next/dynamic";
+import { RainbowButton } from "@/components/ui/rainbow-button";
+import Loading from "@/components/common/loading";
+
+const Spline = dynamic(() => import("@splinetool/react-spline/next"), {
+  ssr: true,
+  loading: () => <Loading />,
+});
 
 export default function Sahayak() {
   return (
-    <div className="relative min-h-screen bg-white">
+    <div className="relative h-screen w-full bg-white overflow-hidden">
       <div className="fixed top-4 right-4 z-50 flex items-center gap-3">
-        <LanguageSwitcher />
         <Link href="/auth/student" className="inline-block">
-          <MovingBorderButton>Login</MovingBorderButton>
+          <RainbowButton>Login</RainbowButton>
         </Link>
+      </div>
+      <div className="h-full w-full flex items-center justify-center">
+        <Spline scene="https://prod.spline.design/H6ABeSZWeDwQPUd7/scene.splinecode" />
       </div>
     </div>
   );
